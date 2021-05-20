b58cbb41d07c430fb6299e1ad039455b00de6563
WALID DAHMANE <DAHMANE_WALID@emsi-edu.ma>
2021-05-20 12:14:31 +0200

    Dependencies Injection Commit

---
+++ eclipse-workspace/TPIOC/src/ext/DaoImplV2.java
@@ -0,0 +1,21 @@
+package ext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import dao.IDao;
+
+@Repository
+public class DaoImplV2 implements IDao{
+
+	@Override
+	public double getData() {
+		/*
+		 * Version WS
+		 */
+		System.out.println("version web service");
+		double data=12;
+		return data;
+	}
+
+}
